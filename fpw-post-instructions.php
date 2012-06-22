af955edad872def0c9d317965244f4153403ff35
frankpw <frankpw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-22 21:03:46 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fpw-post-instructions/trunk@562692 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fpw-post-instructions.php
+++ fpw-post-instructions.php
@@ -25,9 +25,28 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+//	prevent direct access
+if ( preg_match( '#' . basename(__FILE__) . '#', $_SERVER[ 'PHP_SELF' ] ) )  
+	die( "Direct access to this script is forbidden!" );
+
+//	load FPW library
+if ( !function_exists( 'fpw_is_wp_version' ) )
+	require_once dirname(__FILE__) . '/lib/fpwlib.php';
+
+//	quit if not wp 3.3 or higher
+if ( ! fpw_is_wp_version( '3.3' ) ) 
+	wp_die( '<center>Cannot activate! <strong>FPW Post Instructions</strong> plugin ' . 
+			'requires WordPress version <strong>3.3 or higher</strong>!<br />&nbsp;<br />' . 
+			'<a style="border: solid 1px #000; padding: 5px 20px ' . 
+			'5px 20px; border-radius: 10px; -moz-border-radius: 10px; ' . 
+			'-webkit-border-radius: 10px; text-decoration: none; color: black; ' . 
+			'background-color: cyan;" href="/wp-admin/plugins.php" title="Go back to Installed plugins">' . 
+			'Back</a></center>' );
+
 //	back end only
 if ( is_admin() ) {
+	global $fpw_PI;
 	require_once dirname(__FILE__) . '/classes/fpw-post-instructions.class.php';
-	new fpwPostInstructions( dirname(__FILE__), '1.3.0' );
+	$fpw_PI = new fpwPostInstructions( dirname(__FILE__), '1.3.0' );
 }
 ?>
