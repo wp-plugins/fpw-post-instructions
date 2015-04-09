3f48bcc0d47e33dff59bbe31499c8c7f7a58d78b
frankpw <frankpw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-09 00:32:46 +0000

    Added help33.php
    git-svn-id: https://plugins.svn.wordpress.org/fpw-post-instructions/trunk@1130759 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ help/help33.php
@@ -0,0 +1,41 @@
+<?php
+		global	$current_screen;
+
+		$sidebar =	'<p style="font-size: larger">' . __( 'More information', 'fpw-fpi' ) . '</p>' . 
+					'<blockquote><a href="http://fw2s.com/fpw-post-instructions-plugin/" target="_blank">' . __( 'Plugin\'s site', 'fpw-fpi' ) . '</a></blockquote>' . 
+					'<p style="font-size: larger">' . __( 'Support', 'fpw-fpi' ) . '</p>' . 
+					'<blockquote><a href="http://wordpress.org/tags/fpw-post-instructions?forum_id=10" target="_blank">WordPress</a><br />' . 
+					'<a href="http://fw2s.com/support/fpw-post-instructions-support/" target="_blank">FWSS</a></blockquote>'; 
+			
+		$current_screen->set_help_sidebar( $sidebar );
+
+		$intro =	'<p style="font-size: larger">' . __( 'Introduction', 'fpw-fpi' ) . '</p>' . 
+					'<blockquote style="text-align: justify"><p>' . 
+					__( 'New features in WordPress 3.0+ as custom post types and post formats introduced some challenges to webmasters of corporate websites with many users holding post publishing / editing privileges.', 'fpw-fpi' ) . ' ' .
+					__( 'Especially post formats which use the standard post editing screens but may have certain rules imposed by the implementation.', 'fpw-fpi' ) . '</p><p>' . 
+					__( 'Webmasters could try the following options: make a course / training for editors, distribute instructions for editors in a form of printed material, or give editors a link to the documentation. All these methods are good based on one assumption that editors will remember where to find this information. Unfortunately information obtained during the course can be forgotten, printed materials lost or misplaced, and, as everybody knows, people tend to avoid reading documentation.', 'fpw-fpi' ) . '</p><p>' . 
+					__( 'This plugin adds a metabox to post editing screens with special instructions for editors. You can make it as eye-catching as you want ( html in the content is allowed ). It will show on every post editing screen so it cannot be forgotten or misplaced.', 'fpw-fpi' ) . 
+					'</p></blockquote>';
+
+		$current_screen->add_help_tab( array(
+   			'title'   => __( 'Introduction', 'fpw-fpi' ),
+    		'id'      => 'fpw-fpi-help-introduction',
+	   		'content' => $intro,
+		) );
+	
+		$options =	'<p style="font-size: larger">' . __( 'Options', 'fpw-fpi' ) . '</p><blockquote style="text-align: justify"><strong>' . 
+					__( 'Remove plugin\'s data from database on uninstall', 'fpw-fpi' ) . '</strong> (' . __( 'checked', 'fpw-fpi' ) . ') - ' . 
+					__( 'during uninstall procedure all plugin\'s information will be removed from the database', 'fpw-fpi' ) . 
+					'<br /><strong>' . __( 'Add this plugin to the Admin Bar', 'fpw-fpi' ) . '</strong> ( ' . __( 'checked', 'fpw-fpi' ) . ') - ' . 
+					__( 'the plugin\'s link to its settings page will be added to the Admin Bar', 'fpw-fpi' ) . ' ( WordPress 3.1+ )<br /><strong>' . 
+					__( 'Activate visual editor for:', 'fpw-fpi' ) . '</strong> <em>' . __( 'radio buttons for each post type', 'fpw-fpi' ) . '</em> (' . 
+					__( 'checked', 'fpw-fpi' ) . ') - ' . 
+					__( 'allows rich text editing for post type selected', 'fpw-fpi' ) . ' ( WordPress ' . __( 'version', 'fpw-fpi' ) . 
+					' < 3.3, '. __( 'not used in WordPress 3.3+ which allows more than one rich text editor', 'fpw-fpi' ) . ' )</blockquote>';
+
+		$current_screen->add_help_tab( array(
+   			'title'   => __( 'Options', 'fpw-fpi' ),
+    		'id'      => 'fpw-fpi-help-options',
+	   		'content' => $options,
+		) );
+?>
